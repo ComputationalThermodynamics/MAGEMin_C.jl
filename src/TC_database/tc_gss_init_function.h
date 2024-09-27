1093a45d0737d4bb9c2ed566e9145e24de225f92
Nicolas Riel <50235786+NicolasRiel@users.noreply.github.com>
2024-09-27 14:52:27 +0200

    MAGEMin_C v1.5.8 (#56)
    - Added upper mantle to uppermost mantle database (Holland et al., 2013)
    - corrected computation of apfu for pure phases
    - changed simplex threshold from 1e10 to 0 -> fixes issues when dealing with highly reduced systems
    - corrected mistake in handling pseudocompounds for the metabasite database
    - added several pseudocompounds to improve quality of phase equilibrium for igneous and alkaline database

---
+++ src/TC_database/tc_gss_init_function.h
@@ -0,0 +1,34 @@
+/*@ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ **
+ **   Project      : MAGEMin
+ **   License      : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+ **   Developers   : Nicolas Riel, Boris Kaus
+ **   Contributors : Dominguez, H., Assunção J., Green E., Berlie N., and Rummel L.
+ **   Organization : Institute of Geosciences, Johannes-Gutenberg University, Mainz
+ **   Contact      : nriel[at]uni-mainz.de, kaus[at]uni-mainz.de
+ **
+ ** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ @*/
+#ifndef __TC_GSS_INIT_FUNCTION_H_
+#define __TC_GSS_INIT_FUNCTION_H_
+
+#include "../MAGEMin.h"
+#include "../initialize.h"
+								
+void TC_SS_init_mp(	                SS_init_type 		*SS_init,
+									global_variable 	 gv				);				
+void TC_SS_init_mb(	                SS_init_type 		*SS_init,
+									global_variable 	 gv				);
+void TC_SS_init_ig(	                SS_init_type 		*SS_init,
+									global_variable 	 gv				);
+void TC_SS_init_igad(	            SS_init_type 		*SS_init,
+									global_variable 	 gv				);
+void TC_SS_init_um(	                SS_init_type 		*SS_init,
+									global_variable 	 gv				);
+void TC_SS_init_um_ext(	            SS_init_type 		*SS_init,
+									global_variable 	 gv				);
+void TC_SS_init_mtl(	            SS_init_type 		*SS_init,
+									global_variable 	 gv				);
+void TC_SS_init(	        	    SS_init_type 		*SS_init,
+									global_variable 	 gv				);
+
+#endif
