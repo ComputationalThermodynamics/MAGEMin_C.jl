ffb13a296db56106caad26189d1cc2d0e6d2d542
Documenter.jl <documenter@juliadocs.github.io>
2025-06-03 06:29:30 +0000

    build based on d4b7ad4

--- dev/assets/contributing.md.COBbmuEI.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as n}from"./chunks/framework.D35As6yu.js";const l=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"contributing.md","filePath":"contributing.md","lastUpdated":null}'),a={name:"contributing.md"};function r(o,c,i,s,d,p){return n(),e("div")}const m=t(a,[["render",r]]);export{l as __pageData,m as default};
