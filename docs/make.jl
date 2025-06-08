01e8ef9a7b9bd158bae56579974e43d9b203c36a
Nicolas Riel <50235786+NicolasRiel@users.noreply.github.com>
2025-06-08 18:23:17 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -43,7 +43,6 @@ makedocs(;
 deploydocs(;
     repo            = "github.com/ComputationalThermodynamics/MAGEMin_C.jl",
     target          = "dev", # this is where Vitepress stores its output
-    branch          = "gh-pages",
     devbranch       = "main",
     push_preview    = true,
 )
