13e838b229c9d7d8edd1d2a21ff7e64668460478
Nicolas Riel <a.nicolas.riel@gmail.com>
2026-02-17 12:48:16 +0100

    added molar mass of Fe

--- julia/MAGEMin_wrappers.jl
+++ julia/MAGEMin_wrappers.jl
@@ -46,8 +46,8 @@ include("name_solvus.jl")
     Function to retrieve the molar mass of an oxide
 """
 function get_molar_mass( oxide :: String)
-    ref_ox          = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "Fe2O3"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "MnO"; "H2O"; "CO2"; "S"; "P2O5"];
-	ref_MolarMass   = [60.08; 101.96; 56.08; 40.30; 71.85; 159.69; 94.2; 61.98; 79.88; 16.0; 151.99; 70.937; 18.015; 44.01; 32.06; 141.9445];      #Molar mass of oxides
+    ref_ox          = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "Fe2O3"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "MnO"; "H2O"; "CO2"; "S"; "P2O5"; "Fe"];
+	ref_MolarMass   = [60.08; 101.96; 56.08; 40.30; 71.85; 159.69; 94.2; 61.98; 79.88; 16.0; 151.99; 70.937; 18.015; 44.01; 32.06; 141.9445; 55.85];      #Molar mass of oxides
 
     id_oxide        = findfirst(==(oxide), ref_ox)
 
